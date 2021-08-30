2c686a18c095059c6e435fa98e32b7e15bb4aea4
joycezhang <787027175@qq.com>
2021-08-30 15:38:00 +0800

    配置文件

---
+++ server/config/hashids.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Copyright (c) Vincent Klaiber.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://github.com/vinkla/laravel-hashids
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Connection Name
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the connections below you wish to use as
+    | your default connection for all work. Of course, you may use many
+    | connections at once using the manager class.
+    |
+    */
+
+    'default' => 'main',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Hashids Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the connections setup for your application. Example
+    | configuration has been included, but you may add as many connections as
+    | you would like.
+    |
+    */
+
+    'connections' => [
+
+        'main' => [
+            'salt' => env('APP_KEY'),
+            'length' => '32',
+            // 'alphabet' => 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+        ],
+
+        'alternative' => [
+            'salt' => env('APP_KEY'),
+            'length' => '16',
+            // 'alphabet' => 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+        ],
+    ],
+    'middleware' => [
+        'open' => true,
+        // 路由中需要被 decode 的 id
+        'route_parameters' => [],
+        // 请求参数需要被 decode 的 id
+        'request_parameters' => [
+            'manage_id',
+            'dept_id'
+        ]
+    ]
+
+];

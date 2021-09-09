3ac9ea1a45b571a40ac1cc951b67be90595ef9e3
joycezhang <787027175@qq.com>
2021-09-09 17:37:30 +0800

    树形顶级id

--- web/vue2/src/config/env.js
+++ web/vue2/src/config/env.js
@@ -1,6 +1,8 @@
-const config={
-    appName:'LaravelVueAdmin'
+const config = {
+    appName: 'LaravelVueAdmin'
 }
 // 路由模式
 export const routerMode = "history";
+//菜单顶级ID，如需加密id，设置为后端加密后的id，否则设置为0
+export const treeParentId = "3vXAb8agzMlmO0kVA2WyJ5EoNjew9d1r";
 export default config

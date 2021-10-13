a0c676c39131b114d0b056250dac564736b5d5e1
joycezhang <787027175@qq.com>
2021-10-13 14:52:25 +0800

    定义错误页面

---
+++ server/resources/views/errors/501.blade.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge,chrome=1">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>500</title>
+    <link type="text/css" rel="stylesheet" href="/static/css/reset.css"/>
+    <link type="text/css" rel="stylesheet" href="/static/css/error.css"/>
+    <!-- [if lt IE 9]-->
+    <script type="text/javascript" src="/static/ac/lib/html5shiv.min.js"></script>
+    <script type="text/javascript" src="/static/ac/lib/respond.min.js"></script>
+    <!-- [endif] -->
+</head>
+<body>
+<div class="app-result app-result-500">
+    <div class="app-result-icon app-result-image" style="height: 190px">
+        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="230px" height="188px" viewBox="0 0 230 188">
+
+            <!-- SHADE -->
+            <ellipse opacity="0.05" cx="115.143" cy="179.5" rx="115" ry="7.5" />
+
+
+            <!-- BACKGROUND GREY-->
+            <path fill="#FFFFFF" d="M215.143,174.44c0,2.75-2.25,5-5,5h-190c-2.75,0-5-2.25-5-5v-155h200V174.44z" />
+
+
+            <!-- BACKGROUND YELLOW -->
+            <path fill="#FFDC00" d="M215.143,174.44c0,2.75-2.25,5-5,5h-190c-2.75,0-5-2.25-5-5v-155h200V174.44z" />
+
+
+            <!-- CLIPPING MASK -->
+            <defs>
+                <path id="clipmaskshape" d="M215.143,174.44c0,2.75-2.25,5-5,5h-190c-2.75,0-5-2.25-5-5v-155h200V174.44z" />
+            </defs>
+
+            <clipPath id="clipmask">
+                <use xlink:href="#clipmaskshape" overflow="visible" />
+            </clipPath>
+
+
+            <!-- CLIPPING MASK -->
+            <g clip-path="url(#clipmask)">
+                <g>
+                    <rect x="15.143" y="294.44" fill="#FFFFFF" width="200" height="45" />
+                    <rect x="15.143" y="236.19" fill="#E6E6E6" width="200" height="58.25" />
+                    <rect x="15.143" y="19.44" fill="#EBCB00" width="200" height="45" />
+                    <rect x="40.143" y="69.44" fill="#FFFFFF" enable-background="new" width="150" height="35" />
+                    <rect x="40.143" y="246.19" opacity="0.8" fill="#FFFFFF" enable-background="new" width="150" height="35" />
+                    <rect x="40.143" y="170.774" fill="#FFFFFF" width="126.333" height="10" />
+                    <rect x="40.143" y="114.44" opacity="0.8" fill="#FFFFFF" enable-background="new" width="73" height="46.334" />
+                    <rect x="117.143" y="114.44" opacity="0.8" fill="#FFFFFF" enable-background="new" width="73" height="46.334" />
+                    <rect x="91.476" y="185.774" opacity="0.8" fill="#FFFFFF" enable-background="new" width="47.334" height="40" />
+                    <rect x="101.476" y="314.94" opacity="0.8" fill="#FFFFFF" enable-background="new" width="27.334" height="4" />
+                    <rect x="142.809" y="185.774" opacity="0.8" fill="#FFFFFF" enable-background="new" width="47.334" height="40" />
+                    <rect x="40.143" y="185.774" opacity="0.8" fill="#FFFFFF" enable-background="new" width="47.333" height="40" />
+                    <polygon points="124.643,47.425 115.144,52.91 105.643,47.425 105.643,36.456 115.144,30.97 124.643,36.456" />
+                    <rect x="91.476" y="309.94" fill="#00C49A" width="48" height="14" />
+
+                    <animateTransform attributeName="transform" type="translate" fill="freeze" repeatCount="indefinite" begin="0s" dur="6s" keyTimes="0; 0.15; 0.2; 0.7; 0.9; 1" values="0,0;
+			          0,0;
+							  0,-160;
+							  0,-160;
+							  0,0;
+							  0,0" />
+                    <animate attributeName="opacity" fill="freeze" repeatCount="indefinite" begin="0s" dur="6s" keyTimes="0; 0.4; 0.7; 0.9; 1" values="1;
+							  1;
+							  0;
+							  0;
+							  1;" />
+                </g>
+            </g>
+
+            <!-- SCROLL BAR -->
+            <path opacity="0.2" d="M211.143,96.95c0,1.1-0.896,1.99-2,1.99l0,0c-1.104,0-2-0.891-2-1.99V25.432c0-1.1,0.896-1.991,2-1.991l0,0c1.104,0,2,0.892,2,1.991V96.95z">
+                <animateTransform attributeName="transform" type="translate" fill="freeze" repeatCount="indefinite" begin="0s" dur="6s" keyTimes="0; 0.15; 0.2; 0.7; 0.9; 1" values="0,0;
+		          0,0;
+						  0,76;
+						  0,76;
+						  0,0;
+							0,0" />
+            </path>
+
+
+            <!-- WINDOW TOP -->
+            <g>
+                <path fill="#F2F2F2" d="M215.143,19.44h-200v-15c0-2.75,2.25-5,5-5h190c2.75,0,5,2.25,5,5V19.44z" />
+                <rect x="59.143" y="5.44" fill="#FFFFFF" width="151" height="8" />
+                <circle fill="#D90033" cx="24.143" cy="9.44" r="4" />
+                <circle fill="#FFDD00" cx="37.143" cy="9.44" r="4" />
+                <circle fill="#3AD900" cx="50.143" cy="9.44" r="4" />
+            </g>
+
+
+            <!-- TAP -->
+            <circle fill="#FFFFFF" fill-opacity="0.5" opacity="0" stroke-width="4" stroke="#FFFFFF" stroke-miterlimit="10" cx="115.143" cy="157" r="30">
+                <animate attributeName="r" fill="freeze" repeatCount="indefinite" begin="0s" dur="6s" keyTimes="0; 0.4; 0.5; 1" values="0;
+		          0;
+						  80;
+						  80" />
+                <animate attributeName="opacity" fill="freeze" repeatCount="indefinite" begin="0s" dur="6s" keyTimes="0; 0.4; 0.45; 1" values="1;
+						  1;
+						  0;
+						  0;" />
+            </circle>
+
+            <!-- CURSOR -->
+            <circle opacity="0.8" fill="#CDF6ED" cx="115" cy="104.564" r="5.5">
+                <animate attributeName="cx" fill="freeze" repeatCount="indefinite" begin="0s" dur="6s" keyTimes="0; 0.15; 0.2; 0.35; 0.4; 1" values="115;
+		          209.143;
+		          209.143;
+						  115;
+						  115;
+							115" />
+                <animate attributeName="cy" fill="freeze" repeatCount="indefinite" begin="0s" dur="6s" keyTimes="0; 0.15; 0.2; 0.35; 0.4; 1" values="104.564;
+		          61.19;
+		          115.143;
+						  157;
+						  157;
+							157" />
+                <animate attributeName="r" fill="freeze" repeatCount="indefinite" begin="0s" dur="6s" keyTimes="0; 0.15; 0.16; 0.2; 0.22; 0.35; 0.375; 0.4; 1" values="5.5;
+              5.5;
+		  		    4;
+		  		    4;
+		          5.5;
+		          5.5;
+		          4;
+		          5.5;
+						  5.5" />
+                <animate attributeName="opacity" fill="freeze" repeatCount="indefinite" begin="0s" dur="6s" keyTimes="0; 0.05; 0.4; 0.45; 1" values="0;
+		          0.8;
+		          0.8;
+		          0;
+						  0" />
+            </circle>
+        </svg>
+    </div>
+    <div class="app-result-title">温馨提示</div>
+    <div class="app-result-subtitle">{{$exception->getMessage()}}</div>
+</div>
+</body>
+</html>

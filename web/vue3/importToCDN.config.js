dd6f4eb2307465bd23642ad541ddc6eb96827719
joycezhang <787027175@qq.com>
2022-05-15 22:23:39 +0800

    构建项目时，cdn 导入

---
+++ web/vue3/importToCDN.config.js
@@ -0,0 +1,35 @@
+'use strict'
+/**
+ * 构建时，导入cdn
+ */
+module.exports = {
+    baseCDNUrl: '//unpkg.com',
+    cdns: [
+        {
+            name: 'vue',
+            var: 'Vue',
+            path: '//unpkg.com/vue@3.2.33/dist/vue.runtime.global.prod.js'
+        },
+        {
+            name: 'vue-router',
+            var: 'VueRouter',
+            path: '//unpkg.com/vue-router@4/dist/vue-router.min.js'
+        },
+        {
+            name: 'axios',
+            var: 'axios',
+            path: '//unpkg.com/axios@0.27.2/dist/axios.min.js'
+        },
+        {
+            name: 'dayjs',
+            var: 'dayjs',
+            path: '//unpkg.com/dayjs@1.11.2/dayjs.min.js'
+        },
+        {
+            name: 'element-plus',
+            var: 'ElementPlus',
+            path: '//unpkg.com/element-plus',
+            css: '//unpkg.com/element-plus/dist/index.css'
+        }
+    ]
+}

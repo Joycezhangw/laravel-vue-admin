206c35b01cc10e906eb9706785e013deb709fa14
joycezhang <787027175@qq.com>
2022-05-15 22:15:40 +0800

    注入 index.html ejs 模版的数据

--- web/vue3/vite.config.js
+++ web/vue3/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import importToCDN from 'vite-plugin-cdn-import'
+
+import { createHtmlPlugin } from "vite-plugin-html";
+
 import path from 'path'
 
 //当前执行node命令时文件夹的地址（工作目录）
 const root = process.cwd();
-
 /**
  * 路径查找
  * @param {*} dir 
@@ -16,29 +18,31 @@ function resolve(dir) {
 }
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const { VITE_PORT } = loadEnv(mode, root);
-  console.log(VITE_PORT)
-
+export default defineConfig(async ({ mode }) => {
+  const { VITE_PORT, VITE_IS_IMPORT_TO_CDN, VITE_APP_TITLE } = loadEnv(mode, root);
+  //cdn配置
+  const buildImportToCNDConf = require(resolve('./importToCDN.config.js'));
   return {
     plugins: [
       vue(),
       importToCDN({
-        modules: [
-          {
-            name: 'vue',
-            var: 'Vue',
-            path: "https://unpkg.com/vue@3.2.33/dist/vue.runtime.global.prod.js"
-          },
-          {
-            name: 'element-plus',
-            var: 'ElementPlus',
-            path: '//unpkg.com/element-plus',
-            css: '//unpkg.com/element-plus/dist/index.css'
-          }
-        ],
+        modules: parseInt(VITE_IS_IMPORT_TO_CDN) === 1 ? buildImportToCNDConf.cdns : []
       }),
+      createHtmlPlugin({
+        inject: {//需要注入 index.html ejs 模版的数据
+          data: {
+            appTitle: VITE_APP_TITLE,
+          },
+        },
+      })
     ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          charset: false
+        }
+      }
+    },
     resolve: {
       alias: {
         '~': resolve('/'),
@@ -51,7 +55,18 @@ export default defineConfig(({ mode }) => {
       //端口
       port: VITE_PORT,
       //服务启动时是否自动打开浏览器
-      open: true
-    }
+      open: true,
+      sourcemap: false,
+      polyfillDynamicImport: false // 必须为false
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          chunkFileNames: "static/js/[name]-[hash].js",
+          entryFileNames: "static/js/[name]-[hash].js",
+          assetFileNames: "static/[ext]/[name]-[hash].[ext]",
+        },
+      },
+    },
   }
 })

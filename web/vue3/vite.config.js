9ffd04181a24fafa913361c0e0f0f55f57b02011
joycezhang <787027175@qq.com>
2022-06-07 17:53:59 +0800

    配置api代理

--- web/vue3/vite.config.js
+++ web/vue3/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import createVitePlugins from './vite/plugins'
 import { wrapperEnv } from './vite'
+import { createProxy } from './vite/server/proxy'
 import path from 'path'
 
 
@@ -15,10 +16,15 @@ function resolve(dir) {
 export default defineConfig(async ({ mode, command }) => {
   //当前执行node命令时文件夹的地址（工作目录）
   const root = process.cwd();
-  const env = loadEnv(mode, root)
+  //获取到的全部都是string配置，node无法识别其他类型配置，比如 Array | Bool
+  const env = loadEnv(mode, root);
+  //将env转化成node可识别的配置
   const viteEnv = wrapperEnv(env)
   const isBuild = command === 'build';
-  const { VITE_PORT } = env;
+
+  const { VITE_PORT, VITE_PROXY } = viteEnv;
+
+
   return {
     plugins: createVitePlugins(viteEnv, isBuild),
     css: {
@@ -32,13 +38,15 @@ export default defineConfig(async ({ mode, command }) => {
       alias: {
         '~': resolve('/'),
         '@': resolve('src')
-      }
+      },
+      extensions: ['.js', '.json', '.ts', '.vue'], // 使用路径别名时想要省略的后缀名，可以自己 增减
     },
     server: {
       //是否开启https
       https: false,
       //端口
       port: VITE_PORT,
+      proxy: createProxy(VITE_PROXY),
       //服务启动时是否自动打开浏览器
       open: true,
       sourcemap: false,
--- web/vue3/vite/server/proxy.js
+++ web/vue3/vite/server/proxy.js
@@ -1,3 +1,21 @@
-export function createProxy(){
-    const httpsRE = /^https:\/\//;
+/**
+ * api 请求代理
+ * @param {Array} proxyList 
+ * @returns 
+ */
+export function createProxy(proxyList = []) {
+    const httpsReg = /^https:\/\//;
+    const httpProxy = [];
+    for (const [prefix, target] of proxyList) {
+        //判断是否ssl
+        const isHttps = httpsReg.test(target);
+
+        httpProxy[prefix] = { // 需要代理的路径   例如 '/api'
+            target: `${target}/`,
+            changeOrigin: true,
+            rewrite: (path) => path.replace(new RegExp(`^${prefix}`), `${prefix}`),
+            ...(isHttps ? { secure: false } : {}),
+        }
+    }
+    return httpProxy;
 }

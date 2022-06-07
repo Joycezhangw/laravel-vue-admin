97df69576c454f62e4c791fb0d6fe9d5f17cdfff
joycezhang <787027175@qq.com>
2022-06-07 18:01:51 +0800

    配置文件

---
+++ web/vue3/src/landao/config.js
@@ -0,0 +1,33 @@
+
+
+//开发模式
+const isDev = import.meta.env.MODE === "development";
+
+//host
+const host = import.meta.env.VITE_APP_HOST;
+
+//请求地址
+const baseURL = import.meta.env.VITE_APP_BASE_API
+
+//忽略规则
+const ignore = {
+    //不显示进度条请求
+    NProgress: ['/v1/passport/captcha'],
+    //api不需要携带token请求
+    apiToken: ['/v1/passport/captcha', '/v1/passport/login'],
+    //不需要token的页面
+    pageToken: ['./login']
+}
+
+const app={
+    router:{
+        //模式
+        mode:"history",
+        //页面
+        pages:[],
+        //试图 / 路由下的children
+        views:[]
+    }
+}
+
+export { isDev, host, baseURL, ignore,app }

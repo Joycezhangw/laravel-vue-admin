29c54a0c13738be616584e49244934f31575ffbb
joycezhang <787027175@qq.com>
2022-06-20 16:40:22 +0800

    增加 route hook

--- web/vue3/src/landao/hooks/index.js
+++ web/vue3/src/landao/hooks/index.js
@@ -1,5 +1,25 @@
+import { useRefs } from './dom/useRefs'
+import { useRouter, useRoute } from "vue-router";
+
+function useLanDao() {
+    const { refs, setRefs } = useRefs();
+
+    // 路由
+    const route = useRoute();
+
+    // 路由器
+    const router = useRouter();
+
+    return {
+        route,
+        router,
+        refs,
+        setRefs
+    };
+}
+
+export { useRefs, useLanDao };
 export * from './state/useToggle'
 export * from './state/useBoolean'
 export * from './useRequest'
-export * from './dom/useRefs'
 export * from './useCrypto'

60f0328a98fa66161a1fa9202694c465cbd6c053
joycezhang <787027175@qq.com>
2022-06-20 16:42:37 +0800

    Layout左侧导航布局

---
+++ web/vue3/src/layout/components/sidebar.vue
@@ -0,0 +1,223 @@
+<template>
+  <div class="app-slider">
+    <div class="app-slider__logo">
+      <img src="@/assets/logo.png" />
+      <span v-if="!appStore.isFold || appStore.browser.isMini">{{
+        appStore.appInfo.name
+      }}</span>
+    </div>
+    <div class="app-slider__container">
+      <menu-nav />
+    </div>
+  </div>
+</template>
+<script lang="jsx">
+import { defineComponent, h } from "vue";
+import { useBaseStore } from "@/store";
+import { useLanDao } from "@/landao/hooks";
+
+export default defineComponent({
+  name: "appSidebar",
+  components: {
+    MenuNav: {
+      name: "menuNav",
+      setup() {
+        const { menu: menuStore } = useBaseStore();
+        const { route, router } = useLanDao();
+        //跳转页面
+        function toView(url) {
+          if (url != route.path) {
+            router.push(url);
+          }
+        }
+        return {
+          route,
+          menuStore,
+          toView,
+        };
+      },
+      render(ctx) {
+        const { app: appStore } = useBaseStore();
+        function deepMenu(list, index) {
+          return list
+            .filter((e) => !e.hidden)
+            .map((item) => {
+              let html = null;
+              //目录菜单
+              if (item.menuType === 0) {
+                html = h(
+                  <el-sub-menu></el-sub-menu>,
+                  {
+                    index: String(item.menuId),
+                    key: item.menuId,
+                    popperClass: "app-slider__menu",
+                  },
+                  {
+                    title() {
+                      return (
+                        <div class="wrap">
+                          <span v-show={!appStore.isFold || index != 1}>
+                            {item.meta.title}
+                          </span>
+                        </div>
+                      );
+                    },
+                    default() {
+                      if (item.children) {
+                        return deepMenu(item.children, index + 1);
+                      }
+                    },
+                  }
+                );
+              } else {
+                html = h(
+                  <el-menu-item></el-menu-item>,
+                  {
+                    index: item.path,
+                    key: item.menuId,
+                  },
+                  {
+                    default() {
+                      return (
+                        <div class="wrap">
+                          <span v-show={!appStore.isFold || index != 1}>
+                            {item.meta.title}
+                          </span>
+                        </div>
+                      );
+                    },
+                  }
+                );
+              }
+              return html;
+            });
+        }
+
+        const children = deepMenu(ctx.menuStore.menuGroup);
+
+        return (
+          <div class="app-slider__menu">
+            <el-menu
+              default-active={ctx.route.path}
+              background-color="transparent"
+              collapse-transition={false}
+              collapse={appStore.browser.isMini ? false : appStore.isFold}
+              onSelect={ctx.toView}
+            >
+              {children}
+            </el-menu>
+          </div>
+        );
+      },
+    },
+  },
+  setup() {
+    const { app: appStore } = useBaseStore();
+    return {
+      appStore,
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+.app-slider {
+  height: 100%;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+  background-color: #2f3447;
+  &__logo {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 80px;
+    cursor: pointer;
+
+    img {
+      height: 30px;
+      width: 30px;
+    }
+
+    span {
+      color: #fff;
+      font-weight: bold;
+      font-size: 26px;
+      margin-left: 10px;
+      font-family: inherit;
+      white-space: nowrap;
+    }
+  }
+
+  &__container {
+    height: calc(100% - 80px);
+    overflow-y: auto;
+
+    &::-webkit-scrollbar {
+      width: 0;
+      height: 0;
+    }
+  }
+
+  &__menu {
+    &.el-popper {
+      &.is-light {
+        border: 0;
+      }
+    }
+    .el-menu {
+      border-right: 0;
+      background-color: transparent;
+      &--popup {
+        .icon-svg,
+        span {
+          color: #000;
+        }
+      }
+      .el-sub-menu__title,
+      &-item {
+        &.is-active,
+        &:hover {
+          background-color: #4165d7 !important;
+
+          .icon-svg,
+          span {
+            color: #fff;
+          }
+        }
+      }
+      .el-sub-menu__title,
+      &-item,
+      &__title {
+        color: #eee;
+        letter-spacing: 0.5px;
+        height: 50px;
+        line-height: 50px;
+
+        .wrap {
+          width: 100%;
+        }
+
+        .icon-svg {
+          font-size: 16px;
+        }
+
+        span {
+          display: inline-block;
+          font-size: 12px;
+          letter-spacing: 1px;
+          margin-left: 10px;
+        }
+      }
+
+      &--collapse {
+        .wrap {
+          text-align: center;
+
+          .icon-svg {
+            font-size: 18px;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ web/vue3/src/layout/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="page-layout">
+    <div class="page-layout__left">
+      <sidebar />
+    </div>
+    <div class="page-layout__right"></div>
+  </div>
+</template>
+<script name="Layout" setup>
+import Sidebar from "./components/sidebar.vue";
+</script>
+<style lang="scss" scoped>
+.page-layout {
+  display: flex;
+  background-color: #f7f7f7;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  &__left {
+    overflow: hidden;
+    height: 100%;
+    width: 255px;
+    transition: left 0.2s;
+  }
+
+  &__right {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: calc(100% - 208px);
+  }
+  @media only screen and (max-width: 768px) {
+    .page-layout__left {
+      position: absolute;
+      left: 0;
+      z-index: 9999;
+      transition: transform 0.3s cubic-bezier(0.7, 0.3, 0.1, 1),
+        box-shadow 0.3s cubic-bezier(0.7, 0.3, 0.1, 1);
+    }
+
+    .page-layout__right {
+      width: 100%;
+    }
+
+    &.collapse {
+      .page-layout__left {
+        transform: translateX(-100%);
+      }
+
+      .page-layout__mask {
+        display: none;
+      }
+    }
+  }
+
+  @media only screen and (min-width: 768px) {
+    .page-layout__left,
+    .page-layout__right {
+      transition: width 0.2s ease-in-out;
+    }
+
+    .page-layout__mask {
+      display: none;
+    }
+
+    &.collapse {
+      .page-layout__left {
+        width: 64px;
+      }
+
+      .page-layout__right {
+        width: calc(100% - 64px);
+      }
+    }
+  }
+}
+</style>

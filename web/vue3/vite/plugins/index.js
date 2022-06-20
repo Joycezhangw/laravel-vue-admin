07a2866a5dd890fe469c3dc0ec42dc6465bf84c6
joycezhang <787027175@qq.com>
2022-06-20 16:19:57 +0800

    引入 vueJsx 支持

--- web/vue3/vite/plugins/index.js
+++ web/vue3/vite/plugins/index.js
@@ -1,9 +1,10 @@
 import vue from '@vitejs/plugin-vue'
+import vueJsx from "@vitejs/plugin-vue-jsx";
 import createHtml from './html'
 import createImportToCDN from './importToCDN';
 
 export default function createVitePlugins(viteEnv, isBuild = false) {
-    const vitePlugins = [vue()];
+    const vitePlugins = [vue(), vueJsx()];
     vitePlugins.push(createHtml(viteEnv, isBuild))
     vitePlugins.push(createImportToCDN(viteEnv))
 

96bf2f70991d136d872359c0f7235327d67160a1
joycezhang <787027175@qq.com>
2022-06-24 16:50:12 +0800

    定义 empty 组件

--- web/vue3/src/components/index.js
+++ web/vue3/src/components/index.js
@@ -1,6 +1,8 @@
 import iconSvg from "./icon-svg";
 import ldImage from "./ld-image"
+import ldEmpty from "./ld-empty"
 export default {
 	iconSvg,
-	ldImage
-};
+	ldImage,
+	ldEmpty
+}
---
+++ web/vue3/src/components/ld-empty/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="ld-empty">
+    <div class="ld-empty--image"><icon-svg name="empty-data" /></div>
+    <p class="ld-empty--description">{{description}}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "LdEmpty",
+  props: {
+    description: {
+      type: String,
+      default: "暂无数据",
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.ld-empty {
+  margin: 32px 0;
+  color: rgba(0, 0, 0, 0.25);
+  font-size: 14px;
+  line-height: 22px;
+  text-align: center;
+  &--image {
+    height: 40px;
+    margin-bottom: 8px;
+    svg {
+      height: 100%;
+      margin: auto;
+    }
+    .icon-svg {
+      width: 3rem;
+      height: 3rem;
+    }
+  }
+  &--description {
+    margin: 0;
+  }
+}
+</style>

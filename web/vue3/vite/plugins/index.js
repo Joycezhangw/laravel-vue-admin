ce17d43ac778575373eb37fb0db0a43c849d3168
joycezhang <787027175@qq.com>
2022-06-29 16:26:23 +0800

    自动 按需引入 element-plus

--- web/vue3/vite/plugins/index.js
+++ web/vue3/vite/plugins/index.js
@@ -3,12 +3,21 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import createHtml from './html'
 import createImportToCDN from './importToCDN';
 import { svgBuilder } from './svgBuilder';
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default function createVitePlugins(viteEnv, isBuild = false) {
     const vitePlugins = [
         vue(),
         vueJsx(),
         svgBuilder("./src/icons/svg/"),//全局导入svg
+        AutoImport({//自动按需导入
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({//自动按需导入
+            resolvers: [ElementPlusResolver()],
+        }),
     ];
     vitePlugins.push(createHtml(viteEnv, isBuild))
     vitePlugins.push(createImportToCDN(viteEnv))

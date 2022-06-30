4bfc51019979db58ec32f8a1136e952ea76fe665
joycezhang <787027175@qq.com>
2022-06-30 17:46:34 +0800

    ♻️: update  uploadFile

--- web/vue3/src/landao/service/axios.js
+++ web/vue3/src/landao/service/axios.js
@@ -117,43 +117,93 @@ class Http {
         this.interceptors(instance, options.url)
         return instance(options)
     }
+
+    /**
+     * header Content-type 
+     */
+    contentTypeEnum = {
+        // json
+        JSON: 'application/json;charset=UTF-8',
+        // form-data qs
+        FORM_URLENCODED: 'application/x-www-form-urlencoded;charset=UTF-8',
+        // form-data  upload
+        FORM_DATA: 'multipart/form-data;charset=UTF-8',
+    }
     /**
      * GET 请求
      * @param {String} url  api 请求地址
      * @param {Object} params 请求参数
+     * @param {Object} _object 其他配置信息
      * @returns 
      */
     get(url, params = {}, _object = {}) {
-        _object.method = 'get';
-        console.log({ url, params, ..._object })
-        return this.request({ url, params, ..._object })
+        const object = { method: 'GET', ..._object }
+        return this.request({ url, params, ...object })
     }
     /**
      * POST 提交
      * @param {String} url api 请求地址
      * @param {Object} data 请求参数
+     * @param {Object} _object 其他配置信息
      * @returns 
      */
-    post(url, data = {}) {
-        return this.request({ url, method: 'post', data })
+    post(url, data = {}, _object = {}) {
+        const object = {
+            method: 'POST', ..._object
+        }
+        return this.request({ url, data, ...object })
     }
     /**
      * PUT 提交
      * @param {String} url api 请求地址
      * @param {Object} data 请求参数
+     * @param {Object} _object 其他配置信息
      * @returns 
      */
-    put(url, data = {}) {
-        return this.request({ url, method: 'put', data })
+    put(url, data = {}, _object = {}) {
+        const object = { method: 'PUT', ..._object }
+        return this.request({ url, data, ...object })
     }
     /**
      * DELETE 发放
      * @param {String} url api 请求地址
      * @param {Object} params 请求参数
+     * @param {Object} _object 其他配置信息
      * @returns 
      */
-    delete(url, params) {
-        return this.request({ url, method: 'delete', params })
+    delete(url, params, _object = {}) {
+        const object = { method: 'DELETE', ..._object }
+        return this.request({ url, params, ...object })
+    }
+    uploadFile(url, params, _object = {}) {
+        const formData = new Window.FormData();
+        const customFilename = params.name || 'file';
+        if (params.filename) {
+            formData.append(customFilename, params.file, params.filename);
+        } else {
+            formData.append(customFilename, params.file)
+        }
+        if (params.data) {
+            Object.keys(params.data).forEach((key) => {
+                const value = params.data[key];
+                if (Array.isArray(value)) {
+                    value.forEach((item) => {
+                        formData.append(`${key}[]`, item);
+                    });
+                    return;
+                }
+                formData.append(key, params.data[key]);
+            });
+        }
+        const object = {
+            method: 'POST', headers: {
+                'Content-type': this.contentTypeEnum.FORM_DATA,
+                // @ts-ignore
+                ignoreCancelToken: true,
+            }, ..._object
+        }
+        return this.request({ url, data: formData, ...object })
+
     }
 }
 export default Http

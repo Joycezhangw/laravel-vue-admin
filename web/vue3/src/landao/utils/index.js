c8323b087984354b9c1c19e14d2d7eb8055cbc8b
joycezhang <787027175@qq.com>
2022-07-07 10:56:30 +0800

    ♻️(utils): 新增 deepMerge 深度合并

--- web/vue3/src/landao/utils/index.js
+++ web/vue3/src/landao/utils/index.js
@@ -176,3 +176,12 @@ export function deepTree(list, nodeKey = 'menuId', pKey = 'parentId', order = 'm
 
     return orderBy(newList, order);
 }
+
+//深度合并
+export function deepMerge (src = {}, target = {}) {
+    let key = ''
+    for (key in target) {
+      src[key] = isObject(src[key]) ? deepMerge(src[key], target[key]) : (src[key] = target[key])
+    }
+    return src
+  }

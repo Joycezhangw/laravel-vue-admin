ab96e6feec4548be04c1785a2e45682abd1100d0
joycezhang <787027175@qq.com>
2022-07-11 18:13:13 +0800

    ♻️(Role): 角色api

--- web/vue3/src/service/modules/role.js
+++ web/vue3/src/service/modules/role.js
@@ -1,18 +1,58 @@
-import BaseService from "@/landao/service/base";
+import request from '@/landao/service/request'
 /**
  * 角色相关
  */
-export default class RoleService extends BaseService {
-
-    static get apiUri() {
-        return {
-            getList: '/v1/role/list',//获取角色全部列表
-            getPageList: '/v1/role',//获取角色分页列表
-            store: '/v1/role/store',//添加角色
-            update: '/v1/role/update',//更新角色
-            delete: '/v1/role/delete',//删除角色
-            info: '/v1/role/read',//查看角色详情
-            modifyFiled: ''
-        }
+export default class RoleService {
+    /**
+     * 获取角色列表
+     * @returns 
+     */
+    static list() {
+        return request.get('/v1/role/list')
     }
+
+    /**
+     * 获取角色分页列表
+     * @returns 
+     */
+    static page(params) {
+        return request.get('/v1/role', params)
+    }
+    /**
+     * 获取详情
+     * @param {Number | String} roleId 角色ID
+     * @returns 
+     */
+    static read(roleId) {
+        return request.get(`/v1/role/read/${roleId}`)
+    }
+
+    /**
+     * 更新菜单信息
+     * @param {Number | String} roleId 角色id
+     * @param {Object} data 参数
+     * @returns 
+     */
+    static doUpdate(roleId, data) {
+        return request.put(`/v1/role/update/${roleId}`, data)
+    }
+
+    /**
+     * 创建角色
+     * @param {Object} data 参数
+     * @returns 
+     */
+    static doStore(data) {
+        return request.post('/v1/role/store', data);
+    }
+
+    /**
+     * 删除角色
+     * @param {String | Number} roleId 
+     * @returns 
+     */
+    static doDelete(roleId) {
+        return request.delete(`/v1/role/delete/${roleId}`)
+    }
+
 }

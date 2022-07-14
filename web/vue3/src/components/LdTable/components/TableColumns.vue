bce32069ca7096e29a0fba406defdc58995a1229
joycezhang <787027175@qq.com>
2022-07-14 16:33:50 +0800

    ♻️(LdTable): TableColumn 组件封装

---
+++ web/vue3/src/components/LdTable/components/TableColumns.vue
@@ -0,0 +1,24 @@
+<script lang="jsx">
+import { ElTableColumn } from "element-plus";
+import { createVNode, defineComponent } from "vue";
+import VueTypes from "vue-types";
+import { omit } from "lodash-es";
+
+export default defineComponent({
+  name: "TableColumns",
+  props: {
+    column: VueTypes.object.isRequired,
+  },
+  setup(props, { slots }) {
+    return () => {
+      const { type = "" } = props.column;
+      const attrs = {
+        ...props.column,
+        ...{ reserveSelection: type === "selection" },
+      };
+      //omit 排除掉不需要的属性
+      return createVNode(ElTableColumn, omit(attrs, ["slot"]), slots.default);
+    };
+  },
+});
+</script>

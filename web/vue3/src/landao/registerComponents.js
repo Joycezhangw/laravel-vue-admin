eb657b99b3655faa97d30698e0fda77c2b80ec01
joycezhang <787027175@qq.com>
2022-07-18 12:30:40 +0800

    ♻️(update): 注册elementIcon全局组件，解决使用el组件内置图标不显示

--- web/vue3/src/landao/registerComponents.js
+++ web/vue3/src/landao/registerComponents.js
@@ -1,12 +1,17 @@
-import components from "@/components"
-
+import components from "@/components";
+// 如果您正在使用CDN引入，请删除下面一行。
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
 /**
  * 注册公用组件
- * @param {*} App 
+ * @param {*} App
  */
 export async function registerComponent(App) {
-    for (const [key, component] of Object.entries(components)) {
-        App.component(key, component)
-    }
+  for (const [key, component] of Object.entries(components)) {
+    App.component(key, component);
+  }
+  //TODO，全局注册是解决el-element组件内使用icon不显示问题
+  for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    App.component(key, component);
+  }
 }
